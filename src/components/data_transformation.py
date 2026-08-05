790445416f1ec513785c4607b02cd96f8ac0ec51
Anubhav Trivedi <anubhav.trivedi64@gmail.com>
2026-08-05 23:25:40 +0530

    data transformation half complited

---
+++ src/components/data_transformation.py
@@ -0,0 +1,146 @@
+import sys
+import numpy as np
+import pandas as pd
+from imblearn.combine import SMOTETomek
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.compose import ColumnTransformer
+
+from src.constant import TARGET_COLUMN, SCHEMA_FILE_PATH, CURRENT_YEAR
+from src.entity.config_entity import DataTransformationConfig
+from src.entity.artifact_entity import DataTransformationArtifact, DataIngestionArtifact, DataValidationArtifact
+from src.exception import MyException
+from src.logger import logging
+from src.utils.main_utils import read_yaml_files, save_object, save_numpy
+
+
+
+class DataTransformation:
+    def __init__(self,data_ingestion_artifact: DataIngestionArtifact,
+                 data_transformation_config: DataTransformationConfig,
+                 data_validation_artifact: DataValidationArtifact):
+        try:
+            self.data_ingestion_artifact = data_ingestion_artifact
+            self.data_ingestion_artifact = data_transformation_config
+            self.data_validation_artifact = data_validation_artifact
+            self._schema_config = read_yaml_files(file_path = SCHEMA_FILE_PATH)
+
+        except Exception as e:
+            raise MyException(e,sys)
+
+
+    @staticmethod
+    def read_data(file_path) -> pd.DataFrame:
+        try:
+            return pd.read_csv(file_path)
+
+        except Exception as e:
+            raise MyException(e, sys)
+
+
+    def get_data_transformer_object(self) -> Pipeline:
+        """
+        Creates and return a data ransformation object for data,
+        including gender mapping, dummy variable creation, column renaming,
+        feature scaling, and type adjustments.
+        """
+
+        logging.info("Entered get_data_transformer_object method of DataTransformation class")
+
+        try:
+            # Initialize transformers
+            numeric_transformer = StandardScaler()
+            min_max_scaler = MinMaxScaler()
+            logging.info("Transformer Initialized: StandardScaler-MinMaxScaler")
+
+            # Load schema configurations
+            num_features = self.schema_config['num_features']
+            mm_columns = self._schema_config['mm_cols']
+            logging.info("Cols loaded from schema.")
+
+            # Creating preprocessor pipeline
+           
+            preprocessor = ColumnTransformer(
+                    transformers=[
+                        ('standradScaler',numeric_transformer, num_features),
+                        ('MinMaxScaler',min_max_scaler, mm_columns)
+                    ],
+                    remainder='passthrough'  # Leaves other columns as they are
+                )
+
+        except Exception as e:
+            logging.exception("Exception occured in get_data_transformer_object method of DataTransformation class"
+                              )
+            raise MyException(e,sys) from e
+    def _map_gender_column(self,df):
+        """
+        Map Gender column to 0 for female and 1 for Male
+        """            
+        logging.info("Mapping 'Gender' column to binary values")
+        df['Gender'] = df['Gender'].map({'Female':0,'Male':1}).astype(int)
+        return df
+
+    def _create_dummy_columns(self,df):
+        """
+        Create dummy variables for categorical features.
+        """
+        logging.info("Creating dummy variables for categorical features.")
+        df = pd.get_dummies(df, drop_first=True)
+        return df
+    def _rename_columns(self,df):
+        """"
+        Rename specific columns and ensure integer type for dummy columns.
+        """"
+
+        logging.info("Renaming specific columns aand casting to int")
+        df = df.rename(columns={
+            "Vehicle_Age_< 1 Year": "Vehicle_Age_lt_1_Year",
+            "Vehicle_Age_> 2 Years": "Vehicle_Age_gt_2_yeras"
+        })
+
+        for col in ['Vehicle_Age_lt_1_Year','Vehicle_Age_gt_2_Years','Vehicle_Damage_Yes']:
+            if col in df.columns:
+                df[col] = df[col].astype('int')
+
+        return df
+
+
+    def _drop_id_column(self, df):
+        """Drop the 'id' colmn if it exists."""
+        logging.info("Droppping 'id' column")
+        drop_col = self.schema-config['drop_columns']
+        if drop_col in df.columns:
+            df = df.drop(drop_col, axis = 1)
+
+        return df
+
+    def initiate_data_trnasformation(self) -> DataTransformationArtifact:
+        """ Initiates the data transformation component for pipeline."""
+
+        try:
+            logging.info("Data Transformation Started!!!")
+            if not self.data_validation_artifact.validation-status:
+                raise Exception(self.data_validation_artifact.message)
+
+            # load train and test data
+
+            train_df = self.read_data(file_path = self.data_ingestion_artifact.trained_file_path)
+            test_df = self.read_data(file_path=self.data.ingestion_artifact.test_file_path)
+            logging.info("Train-Test data loaded")
+
+
+            input_features_train_df = train_df.drop(columns=[TARGET_COLUMN],axis = 1)
+            target_feature_train_df = train_df[TARGET_COLUMN]
+
+            input_features_test_df = test_df.drop(columns=['TARGET_COLUMN'],axis = 1)
+            target_feature_train_df = train_df[TARGET_COLUMN]
+
+
+            # Apply custom transformation in specified sequence
+            input_features_train_df = self.map_gender_column(input_features_train_df)
+            input_features_train_df = self.drop_id_column(input_features_train_df)
+            input_features_train_df = self.create_dummy_columns(input_features_train_df)
+            input_features_train_df = self.rename_columns(input_features_train_df)
+       
+
+            
--- src/utils/main_utils.py
+++ src/utils/main_utils.py
@@ -28,5 +28,63 @@ def write_yaml_files(file_path: str, content: object, replace: bool=False) -> No
     except Exception as e:
         raise MyException(e, sys) from e
 
+def load_object(file_path: str) -> object:
+    """
+    Return model/object from project directory.
+    file_path: str location of file to load
+    return: Model/Obj
+    """
+
+    try:
+        with open(file_path, "rb") as file_obj:
+            obj = dill.load(file_obj)
+            return obj
+
+    except Exception as e:
+        raise MyException(e,sys) from e
+
+def save_numpy_array_data(file_path:str, array:np.array):
+    """
+    Save numpy array data to file
+    file_path: str location of file to save
+    array: np.array data to save
+    """
+
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path,'wb') as file_obj:
+            np.save(file_obj, array)
+
+    except Exception as e:
+        raise MyException(e,sys) from e
+def save_object(file_path:str, obj: object)-> None:
+    logging.info("Entered the save_object method of utils")
+
+    try:
+        os.makedirs(os.path.dirname(file_path), exist_ok = True)
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
 
 
+        logging.info("Exited the save_object method of utils")
+
+    except Exception as e:
+        raise MyException(e, sys) from e
+
+# def drop_columns(df: DataFrame, col: list) -> DataFrame:
+#     """
+#     drop the columns form a pandas DataFrame
+#     df: pandas DataFrame
+#     cols: list of columns to be dropped
+#     """
+#     logging.info("Entered drop_columns methon of utils")
+
+#     try:
+#         df = df.drop(columns=cols, axis=1)
+
+#         logging.info("Exited the drop_columns method of utils")
+        
+#         return df
+#     except Exception as e:
+#         raise MyException(e, sys) from e
